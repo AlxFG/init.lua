ea599365c877ca228622a2620b90a56b1346d1d9
Alx <alxfg@purelymail.com>
2023-10-31 22:32:04 +0000

    a few modifications

--- after/plugin/neomarks.lua
+++ after/plugin/neomarks.lua
@@ -2,7 +2,7 @@ marks = require("neomarks")
 marks.setup({
     storagefile = vim.fn.stdpath('data') .. "/neomarks.json",
     menu = {
-        title = "Neomarks",
+        title = "Marks",
         title_pos = "center",
         border = "rounded",
         width = 60,
@@ -17,4 +17,3 @@ vim.keymap.set("n", "<C-h>", function() marks.jump_to(1) end)
 vim.keymap.set("n", "<C-j>", function() marks.jump_to(2) end)
 vim.keymap.set("n", "<C-k>", function() marks.jump_to(3) end)
 vim.keymap.set("n", "<C-l>", function() marks.jump_to(4) end)
-
